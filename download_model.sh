30596a78482aaeb113649d53ea93653fc3072b49
hhsinping <36083700+hhsinping@users.noreply.github.com>
2020-08-23 18:43:51 -0700

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 wget https://www.dropbox.com/s/n7kgm2uk853ya1j/net_first.pth -P ./first
 wget https://www.dropbox.com/s/yp6xhpblga73odz/net_second.pth -P ./second
+mkdir inters
+mkdir outputs
